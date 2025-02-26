bcc50ddcf560ffb0a7d7108cffaed38a935c452a
AhmedALharbi <ahmedfalahalharbi@gmail.com>
2025-02-26 13:58:18 +0300

    add varibales

--- src/Order.java
+++ src/Order.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.*;
+
+enum OrderStatus {
+    PENDING, SHIPPED, DELIVERED
+}
 
 public class Order {
+    private UUID orderId;
+    private String customerName;
+    private UUID productId;
+    private int quantity;
+    private OrderStatus orderStatus;
+
 }
--- src/OrderManager.java
+++ src/OrderManager.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class OrderManager {
+    private final ExecutorService executor = Executors.newFixedThreadPool(3);
+    private final List<Order> orders = new ArrayList<>();
 }

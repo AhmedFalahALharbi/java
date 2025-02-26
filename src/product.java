82eddaa4f57fbef6e81e8bb022059b69fb0f7f9a
AhmedALharbi <ahmedfalahalharbi@gmail.com>
2025-02-26 13:33:22 +0300

    add varibales and functions

--- src/product.java
+++ src/product.java
@@ -1,10 +1,23 @@
+import java.util.UUID;
+
 class Product {
+
+    private UUID productId;
     private String name;
     private double price;
+    private int stockLevel;
+    private int reorderThreshold;
 
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
+    }
+
+    public Product(String name, double price, int stockLevel, int reorderThreshold) {
+        this.name = name;
+        this.price = price;
+        this.stockLevel = stockLevel;
+        this.reorderThreshold = reorderThreshold;
     }
 
     public String getName() {
@@ -23,7 +36,32 @@ class Product {
         this.price = price;
     }
 
-    public void displayProduct() {
-        System.out.println("Product Name: " + name + ", Price: " + price);
+    public int getStockLevel() {
+        return stockLevel;
     }
+
+    public void setStockLevel(int stockLevel) {
+        this.stockLevel = stockLevel;
+    }
+
+    public int getReorderThreshold() {
+        return reorderThreshold;
+    }
+
+    public void setReorderThreshold(int reorderThreshold) {
+        this.reorderThreshold = reorderThreshold;
+    }
+    public void sellProduct(int quantity) {
+        if (quantity>stockLevel) {
+            System.out.println("Sorry, not enough stock");
+        }
+        else {
+            stockLevel -= quantity;
+            System.out.println(quantity + "sold, Remaining: " + stockLevel);
+        }
+    }
+        public void restock(int quantity) {
+            stockLevel += quantity;
+            System.out.println(quantity + "restocked, Remaining: " + stockLevel);
+        }
 }
